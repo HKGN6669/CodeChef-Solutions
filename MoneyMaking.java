011ab6bacd95217c058c6fe5288856a02b09d93b
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-13 20:28:43 +0530

    Add files via upload

---
+++ MoneyMaking.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int d=X*5000+Y*9800;
+		System.out.println(d);
+
+	}
+}
