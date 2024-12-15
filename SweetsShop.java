1d2c0a064a57627bce10e7fdc1dae07d1e2f5938
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-15 11:42:22 +0530

    Add files via upload

---
+++ SweetsShop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int N=sc.nextInt();
+		int f=X-10*N;
+		int J=f/20;
+		System.out.println(J);
+	}
+}
