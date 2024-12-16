1fb984fb313cdce9b67be65548a71cbdeeb81e64
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-16 10:34:53 +0530

    Add files via upload

---
+++ Olympics2024.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int G=sc.nextInt();
+		int S=sc.nextInt();
+		int B=sc.nextInt();
+		int M=5-G+5-S+5-B;
+		System.out.println(M);
+	}
+}
