5f8f2c724e57d99aaabe7dc24d28b0d18c17a717
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-27 23:39:51 +0530

    Add files via upload

---
+++ WaitingTime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    int K=sc.nextInt();
+		    int X=sc.nextInt();
+		    int d=7*K-X;
+		    System.out.println(d);
+		}
+	}
+}
