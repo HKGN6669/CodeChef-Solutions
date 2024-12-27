afc6bae19e104181898c72c2e92200f1cb188ca0
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-27 20:25:09 +0530

    Add files via upload

---
+++ LuckySeven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class codechef{
+    public static void main(String args[]) throws java.lang.Exception{
+        Scanner sc=new Scanner(System.in);
+        int S=sc.nextInt();
+        System.out.println(S.charAt(6));
+    }
+}
