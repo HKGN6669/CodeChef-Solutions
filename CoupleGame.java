61a2cf5c20385518c73ead0adcedc96f231c9c59
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2025-01-22 23:19:44 +0530

    Add files via upload

---
+++ CoupleGame.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    int G=sc.nextInt();
+		    int B=sc.nextInt();
+		    int S=B-G;
+		    System.out.println(S);
+		}
+	}
+}
