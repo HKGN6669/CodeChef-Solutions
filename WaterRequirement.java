d25e071caaa832e6870d9996b68e67dcc1a403e4
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2025-01-23 00:00:21 +0530

    Add files via upload

---
+++ WaterRequirement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    int N=sc.nextInt();
+		    int Y=2*N;
+		    System.out.println(Y);
+		}
+	}
+}
